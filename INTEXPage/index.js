66ca73e18cc06ed4e4b9401cd57235b70e7b1eca
tylerhull14 <tylerdh.hull@gmail.com>
2023-12-06 16:16:06 -0700

    lets try

--- INTEXPage/index.js
+++ INTEXPage/index.js
@@ -157,5 +157,80 @@ app.post("/deleteUser/:id", (req, res) => {
    });
 });
 
+<<<<<<< Updated upstream
+=======
+app.post("/survey", async (req, res) => {
+   try {
+       // Start a transaction
+       await knex.transaction(async trx => {
+           // Validate and sanitize req.body data here
+           // ...
+
+           // Get the current count of persons
+           const rowCount = await trx('persons').count('PersonID as count').first();
+           const peopleCount = rowCount ? rowCount.count + 1 : 1;
+
+           // Handle date and time
+           const now = new Date();
+           const dateTime = now.toISOString();
+
+           // Platform array
+           const platforms = ['cbFacebook', 'cbTwitter', 'cbInstagram', 'cbYouTube', 'cbDiscord', 'cbReddit', 'cbPinterest', 'cbTikTok', 'cbSnapchat'];
+           const recordsToInsert = platforms.map((platform, index) => {
+               if (req.body[platform] === 'on') {
+                   return {
+                       Date: dateTime,
+                       OccupationStatus: req.body.OccupationStatus,
+                       PlatformID: index + 1,
+                       PersonID: peopleCount
+                   };
+               }
+           }).filter(Boolean);
+
+           // Insert into 'records' if any platform is checked
+           if (recordsToInsert.length > 0) {
+               await trx('records').insert(recordsToInsert);
+           }
+
+           // Insert into 'persons'
+           await trx('persons').insert({
+               Age: req.body.Age,
+               Gender: req.body.Gender,
+               RelationshipStatus: req.body.RelationshipStatus,
+               OccupationStatus: req.body.OccupationStatus,
+               UniversityAffiliation: req.body.cbUniversity,
+               SchoolAffiliation: req.body.cbSchool,
+               CompanyAffiliation: req.body.cbCompany,
+               GovernmentAffiliation: req.body.cbGovernment, 
+               PrivateAffiliation: req.body.cbPrivate,
+               SocialMediaUser: req.body.cbSchool, 
+               UsageID: req.body.AvgTime,
+               Q1: req.body.specificPurpose,
+               Q2: req.body.DistractedBusy,
+               Q3: req.body.RestlessWithout,
+               Q4: req.body.EasilyDistracted,
+               Q5: req.body.BotheredByWorries,
+               Q6: req.body.DifficultyToConcentrate,
+               Q7: req.body.CompareYourself,
+               Q8: req.body.FeelAboutComparisons,
+               Q9: req.body.SeekValidation,
+               Q10: req.body.OftenDepressed,
+               Q11: req.body.InterestFluctuate,
+               Q12: req.body.SleepIssues,
+               // AverageScore: req.body.AverageScore,
+               // PlatformCount: recordsToInsert.length,
+               Origin: 'Provo'
+           });
+       });
+
+       // Send a success response
+       res.status(200).send('Survey data submitted successfully');
+   } catch (error) {
+       console.error(error);
+       res.status(500).send('Internal Server Error');
+   }
+});
+
+>>>>>>> Stashed changes
  //listen at the end
 app.listen(port,() => console.log("I am listening"));
